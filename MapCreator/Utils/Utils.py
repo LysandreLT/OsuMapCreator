a7e829fa144f1045a1e0bd556334affdda7c7f28
hb <106774638+BerthonHugo@users.noreply.github.com>
2023-04-06 19:36:19 +0200

    extraction interface

---
+++ MapCreator/Utils/Utils.py
@@ -0,0 +1,166 @@
+import os
+import shutil
+import zipfile
+
+import librosa as librosa
+
+
+def readZip(file: str):
+    with zipfile.ZipFile(file, mode="r") as archive:
+        return archive.namelist()
+
+
+def isArchive(file: str):
+    return zipfile.is_zipfile(file)
+
+
+def isOSZFile(file: str):
+    # return file.split(".")[-1] != "osz"
+    name = file.split(".")
+    if name[-1] != "osz":
+        print(f"File '{file}' is not an .osz file")
+        return False
+    else:
+        return True
+
+    # if zipfile.is_zipfile(fileName):
+    #     with zipfile.ZipFile(fileName, "r") as archive:
+    #         archive.printdir()
+    # else:
+    #     print("File is not an .osz file")
+
+
+def extractAll(file: str, dirPath: str):
+    """
+    :param file: file path of the zip to extract
+    :param dirPath: directory in which the zip will be extracted
+    """
+    with zipfile.ZipFile(file, 'r') as zip:
+        # extracting all the files
+        zip.extractall(path=dirPath)
+
+
+def extract(file: str, zip_path: str, destDirPath: str):
+    """
+    :param file: file in the zipfile to extract
+    :param zip_path : file path of the zip
+    :param destDirPath: directory in which the zip will be extracted
+    """
+    with zipfile.ZipFile(zip_path, 'r') as zip:
+        # extracting all the files
+        zip.extract(member=file, path=destDirPath)
+
+
+# def deleteFileAfterExtraction(file):
+#     if os.path.exists(file):
+#         os.remove(file)
+#     else:
+#         print("The file does not exist")
+#
+#
+def get_all_file_paths(directory):
+    # initializing empty file paths list
+    file_paths = []
+
+    # crawling through directory and subdirectories
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            # join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+
+    # returning all file paths
+    return file_paths
+
+
+def write_archive(file_paths, name: str):
+    """
+    :param file_paths: list of files to zip
+    :param name: name of the zip file
+    """
+    # writing files to a zipfile
+    with zipfile.ZipFile(f'{name}.osz', 'w') as zip:
+        # writing each file one by one
+        for file in file_paths:
+            zip.write(file)
+    print('All files zipped successfully!')
+
+
+def write_osz_archive(directory: str, name: str):
+    """
+    :param directory: directory in which the zip will be created
+    :param name: name of the zip file
+    """
+    # calling function to get all file paths in the directory
+    file_paths = get_all_file_paths(directory)
+    write_archive(file_paths, name)
+
+
+def selectList(list):
+    new_list = []
+    for item in list:
+        ext = item.split(".")
+        if ext[-1] == "osu" or ext[-1] == "mp3" or ext[-1] == "ogg":
+            new_list.append(item)
+
+    return new_list
+
+
+def get_duration(path):
+    y, sr = librosa.load(path, sr=44100)
+    return librosa.get_duration(y=y, sr=sr)
+
+
+def post_treatment(path: str):
+    if os.path.isdir(path):
+        list = os.listdir(path)
+        for file in list:
+            if file.split(".")[-1] != "osu":
+                duration = get_duration(os.path.join(path, file))
+                if duration < 20:
+                    delete_tree(os.path.join(path, file))
+    else:
+        if path.split(".")[-1] != "osu":
+            duration = get_duration(os.path.realpath(path))
+            if duration < 20:
+                delete_tree(path)
+
+
+def clean_archive_folder(path: str, dest: str) -> None:
+    for entry in os.scandir(path):
+        # if file go to clean archive else clean folder again
+        if entry.is_file():
+            if dest.find("/temp"):
+                dest = dest.replace("/temp", "/maps")
+            clean_archive(entry.path, dest)
+        else:
+            clean_archive_folder(entry.path, dest)
+
+
+def delete_tree(path):
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            os.remove(path)
+    else:
+        print("path doesn't exists")
+
+
+def clean_archive(path: str, dest: str):
+    if isArchive(path) and isOSZFile(path):
+        dir_path = dest + "/maps/" + ".".join(os.path.basename(path).split(".")[:-1])
+        if dest.find("/maps/maps") or dest.find("/maps//maps"):
+            dir_path = dir_path.replace("/maps//maps", "/maps").replace("/maps/maps", "/maps")
+
+        clean_list = selectList(readZip(path))
+        if not os.path.exists(dir_path):
+            os.mkdir(dir_path)
+            # os.makedirs(dir_path)
+        for items in clean_list:
+            extract(items, path, dir_path)
+        post_treatment(dir_path)
+    elif isArchive(path):
+        temp_path = dest + "/temp/"
+        extractAll(path, temp_path)
+        clean_archive_folder(temp_path, dest)
---
+++ MapCreator/app/ui/CleanArchives.py
@@ -0,0 +1,149 @@
+import datetime
+import time
+from tkinter.messagebox import askyesno
+from tkinter import filedialog
+import tkinter as tk
+import os
+
+import MapCreator.Utils.Utils as util
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        # dest Dir
+        self.location = os.getcwd()
+
+        # configure the root window
+        self.title('App')
+        self.geometry('480x360')
+
+        # init menu
+        self.create_menu_bar()
+
+        # Location frame
+        frame_output = tk.Frame(self)
+        frame_output.pack(fill=tk.X)
+
+        label_output = tk.Label(frame_output, text="Output :")
+        label_output.pack(side=tk.LEFT, padx=5)
+
+        location_frame = tk.Frame(self)
+        location_frame.pack(fill=tk.X, padx=5)
+
+        lbl1_location = tk.Label(location_frame, text=" Location :", width=6)
+        lbl1_location.pack(side=tk.LEFT, padx=5, pady=5)
+
+        self.entry_location_output = tk.Entry(location_frame)
+        self.entry_location_output.pack(fill=tk.X, padx=5, expand=True)
+        self.entry_location_output.insert(0, self.location)
+
+        # Data frame
+        frame_data = tk.Frame(self)
+        frame_data.pack(fill=tk.X)
+
+        label_data = tk.Label(frame_data, text="Archives or Folder path:")
+        label_data.pack(side=tk.LEFT, padx=5)
+
+        frame_data_location = tk.Frame(self)
+        frame_data_location.pack(fill=tk.X, padx=5)
+
+        lbl1_location = tk.Label(frame_data_location, text=" Location :", width=6)
+        lbl1_location.pack(side=tk.LEFT, padx=5, pady=5)
+
+        self.entry_location_data = tk.Entry(frame_data_location)
+        self.entry_location_data.pack(fill=tk.X, padx=5, expand=True)
+
+        # delete original
+        frame_delete_origin = tk.Frame(self)
+        frame_delete_origin.pack(fill=tk.X)
+
+        label_delete_origin = tk.Label(frame_delete_origin, text="Delete 'Archives or Folder path' after extraction?")
+        label_delete_origin.pack(side=tk.LEFT, padx=5)
+
+        self.is_delete_origin_data = tk.BooleanVar()
+        ChkBttn_delete_origin = tk.Checkbutton(frame_delete_origin, width=15, variable=self.is_delete_origin_data,
+                                               command=lambda: self.confirm(
+                                                   f"Are you sure you want to delete {self.entry_location_output.get()}?"))
+        ChkBttn_delete_origin.pack(side=tk.LEFT, padx=5, pady=5)
+
+        # btn
+        frame = tk.Frame(self, relief=tk.RAISED, borderwidth=1)
+        frame.pack(fill=tk.BOTH, expand=True)
+
+        extract_btn = tk.Button(self, text="Extract", command=self.extract_all)
+        extract_btn.pack(side=tk.RIGHT, padx=5, pady=5)
+
+        extract_btn = tk.Button(self, text="Debug", command=self.debug)
+        extract_btn.pack(side=tk.RIGHT, padx=5, pady=5)
+
+    def create_menu_bar(self):
+        menu_bar = tk.Menu(self)
+        menu_file = tk.Menu(menu_bar, tearoff=0)
+        menu_file.add_command(label="Change Location", command=lambda: self.browse_folder(self.entry_location_output))
+        menu_file.add_command(label="Import Archive", command=lambda: self.browse_file(self.entry_location_data))
+        menu_file.add_command(label="Import Archives", command=lambda: self.browse_folder(self.entry_location_data))
+        menu_file.add_separator()
+        menu_file.add_command(label="Exit", command=self.quit)
+        menu_bar.add_cascade(label="File", menu=menu_file)
+
+        self.config(menu=menu_bar)
+
+    def browse_file(self, entry: tk.Entry):
+        filename = filedialog.askopenfilename(initialdir=self.location,
+                                              title="Select a File",
+                                              filetypes=(("all files", "*.*"), ("Text files", "*.txt*")))
+        entry.delete(0, tk.END)
+        entry.insert(0, filename)
+
+    def browse_folder(self, entry: tk.Entry):
+        self.location = filedialog.askdirectory(initialdir=self.location,
+                                                title="Select a Folder")
+        entry.delete(0, tk.END)
+        entry.insert(0, self.location)
+
+    def extract_all(self) -> None:
+        start = time.time_ns()
+        if not len(self.entry_location_data.get()) == 0:
+            # check if all folder exist
+            if not os.path.exists(self.entry_location_output.get()):
+                os.makedirs(self.entry_location_output.get() + "/maps")
+            # check if folder + 'maps' exist
+            if not os.path.exists(self.entry_location_output.get() + "/maps"):
+                os.mkdir(self.entry_location_output.get() + "/maps")
+
+            # check if data is folder else is file
+            if os.path.isdir(self.entry_location_data.get()):
+                util.clean_archive_folder(self.entry_location_data.get(), self.entry_location_output.get())
+                print("done!")
+            else:
+                util.clean_archive(self.entry_location_data.get(), self.entry_location_output.get())
+        else:
+            print("no path")
+
+        if os.path.exists(os.path.join(self.entry_location_output.get(), "temp")):
+            util.delete_tree(os.path.join(self.entry_location_output.get(), "temp"))
+        if self.is_delete_origin_data.get():
+            if os.path.exists(self.entry_location_data.get()):
+                util.delete_tree(self.entry_location_data.get())
+        end = time.time_ns()
+        time_in_sec = (end - start) / 1000000000
+        print(f"done in {time_in_sec}s")
+
+    def debug(self):
+        print("is dir : ", os.path.isdir(self.entry_location_data.get()))
+        print(self.is_delete_origin_data.get())
+        print(str(datetime.timedelta(seconds=util.get_duration("C:/Users/hugob/dev/python/OSU_AI/MapCreator/app/ui/maps/1953850 RIOT - Overkill/drum-hitclap.ogg"))))
+
+    def confirm(self, message: str):
+        if self.is_delete_origin_data.get():
+            answer = askyesno(title="confirmation", message=message)
+            if not answer:
+                self.is_delete_origin_data.set(False)
+
+
+if __name__ == "__main__":
+    app = App()
+    # Let the window wait for any events
+    app.mainloop()
