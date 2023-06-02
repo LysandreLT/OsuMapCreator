94020d94176e9b25d640ce5ca9b6c223e0ec8171
LysandreLT <125960659+LysandreLT@users.noreply.github.com>
2023-06-02 20:44:41 +0200

    add melspectrogram transpose

--- MapCreator/Utils/audio.py
+++ MapCreator/Utils/audio.py
@@ -14,12 +14,11 @@ def load_melspectrogram(audio_path, plot=False):
         fig.colorbar(img, ax=ax, format='%+2.0f dB')
         ax.set(title='Mel-frequency spectrogram')
         plt.show()
-    return melspectrogram
+    return melspectrogram.transpose()
 
 
 if __name__ == "__main__":
     path = "C:/Users/Lysandre/Documents/GitHub/OsuMapCreator/MapCreator/datasets/maps/33688 DJ Okawari - Flower " \
            "Dance/Flower Dance.mp3"
     mel_spectro = load_melspectrogram(path)
-    print(mel_spectro.shape)
 
