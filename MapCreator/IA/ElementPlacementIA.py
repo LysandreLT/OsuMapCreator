3471d4e962f571fdbce9db0cbd886c08e56e011e
LysandreLT <125960659+LysandreLT@users.noreply.github.com>
2023-06-08 18:45:29 +0200

    Inference (sampling)

--- MapCreator/IA/ElementPlacementIA.py
+++ MapCreator/IA/ElementPlacementIA.py
@@ -1,62 +1,74 @@
-from keras import layers
+import numpy as np
+import tensorflow as tf
 from tensorflow import keras
 
 """
-
 Entrées : 
 Sortie : .txt
 """
 
+latent_dim = 256
+input_dim = 128
 
-def build_model(input_dim, decoder_input_shape, latent_dim):
-    # Model's input
-    input_spectrogram = layers.Input((None, input_dim), name="input")
-    # Expand the dimension to use 2D CNN.
-    x = layers.Reshape((-1, input_dim, 1), name="expand_dim")(input_spectrogram)
-    # Convolution layer 1
-    x = layers.Conv2D(
-        filters=32,
-        kernel_size=[11, 41],
-        strides=[2, 2],
-        padding="same",
-        use_bias=False,
-        name="conv_1",
-    )(x)
-    x = layers.BatchNormalization(name="conv_1_bn")(x)
-    x = layers.ReLU(name="conv_1_relu")(x)
-    # Convolution layer 2
-    x = layers.Conv2D(
-        filters=32,
-        kernel_size=[11, 21],
-        strides=[1, 2],
-        padding="same",
-        use_bias=False,
-        name="conv_2",
-    )(x)
-    x = layers.BatchNormalization(name="conv_2_bn")(x)
-    x = layers.ReLU(name="conv_2_relu")(x)
-    # Reshape the resulted volume to feed the RNNs layers
-    x = layers.Reshape((-1, x.shape[-2] * x.shape[-1]))(x)
+model = keras.models.load_model("MapCreator")
 
-    encoder = keras.layers.LSTM(latent_dim, return_state=True)
-    encoder_outputs, state_h, state_c = encoder(x)
+# Encoder
+encoder_inputs = model.input[0]  # input_1
+encoder_outputs, state_h_enc, state_c_enc = model.layers[10].output  # lstm_1
+encoder_states = [state_h_enc, state_c_enc]
+encoder_model = keras.Model(encoder_inputs, encoder_states)
 
-    # We discard `encoder_outputs` and only keep the states.
-    encoder_states = [state_h, state_c]
+# Decoder
+decoder_inputs = model.input[1]  # input_2
+decoder_state_input_h = keras.Input(shape=(latent_dim,))
+decoder_state_input_c = keras.Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
 
-    # Set up the decoder, using `encoder_states` as initial state.
-    decoder_inputs = keras.Input(shape=(None, decoder_input_shape))
+decoder_lstm = model.layers[11]
+decoder_outputs, state_h_dec, state_c_dec = decoder_lstm(
+    decoder_inputs, initial_state=decoder_states_inputs
+)
+decoder_states = [state_h_dec, state_c_dec]
+decoder_dense = model.layers[12]
+decoder_outputs = decoder_dense(decoder_outputs)
+decoder_model = keras.Model(
+    [decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states
+)
 
-    # We set up our decoder to return full output sequences,
-    # and to return internal states as well. We don't use the
-    # return states in the training model, but we will use them in inference.
-    decoder_lstm = keras.layers.LSTM(latent_dim, return_sequences=True, return_state=True)
-    decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
-    decoder_dense = layers.Dense(13, activation='relu')
-    decoder_outputs = decoder_dense(decoder_outputs)
 
-    # Define the model that will turn
-    # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
-    model = keras.Model([input_spectrogram, decoder_inputs], decoder_outputs)
-    return model
+def sampling(input_seq):
+    # Encode the input as state vectors.
+    states_value = encoder_model.predict(input_seq)
+    print(len(states_value[0]))
+    target_seq = np.array([-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0]).reshape(
+        (1, 1, -1))
 
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sample = []
+    while not stop_condition:
+        output_element, h, c = decoder_model.predict([target_seq] + states_value)
+
+        decoded_sample.append(output_element)
+
+        # Exit condition: either hit max length
+        # or find stop character.
+        if output_element == [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] \
+                or len(decoded_sample) > 4000:
+            stop_condition = True
+
+        # Update the target sequence (of length 1).
+        target_seq = output_element
+
+        # Update states
+        states_value = [h, c]
+    return decoded_sample
+
+
+if __name__ == "__main__":
+    from MapCreator.Utils.trainingMapParser import *
+    base_path = "C:/Users/Lysandre/Documents/GitHub/OsuMapCreator/MapCreator/datasets"
+    paths = get_paths(os.path.join(base_path, "maps"))
+    df, spectrograms, diff = load_beatmaps_and_spectrograms(paths)
+    sampling(spectrograms[0])
--- MapCreator/IA/ElementPlacementIATraining.py
+++ MapCreator/IA/ElementPlacementIATraining.py
@@ -1,15 +1,13 @@
-import numpy as np
-
-from MapCreator.IA.ElementPlacementIA import build_model
+from keras import layers
 from tensorflow import keras
 from MapCreator.Utils.trainingMapParser import *
 
 base_path = "C:/Users/Lysandre/Documents/GitHub/OsuMapCreator/MapCreator/datasets"
 paths = get_paths(os.path.join(base_path, "maps"))
 df, spectrograms, diff = load_beatmaps_and_spectrograms(paths)
-x_train = spectrograms[:int(len(spectrograms) * 0.8)]
+x_train = spectrograms
 x_train = np.array(x_train, dtype=float)
-y_train = df[:int(len(df) * 0.8)]
+y_train = df
 y_train = np.array(y_train, dtype=float)
 decoder_input = np.zeros((len(y_train), 4001, 13))
 
@@ -17,7 +15,7 @@ decoder_input = np.zeros((len(y_train), 4001, 13))
 index = 0
 start_of_sequence = np.array([-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0])
 end_of_sequence = np.array([-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2]).reshape((1, 1, -1))
-end_of_sequence = np.repeat(end_of_sequence, 7, axis=0)
+end_of_sequence = np.repeat(end_of_sequence, len(y_train), axis=0)
 
 for sublist in decoder_input:
     sublist[0] = start_of_sequence
@@ -26,27 +24,78 @@ for sublist in decoder_input:
 y_train = np.append(y_train, end_of_sequence, axis=1)
 print(y_train[0][-1])
 
-x_val = spectrograms[int(len(spectrograms) * 0.8):]
-x_val = np.array(x_val, dtype=float)
-y_val = df[int(len(df) * 0.8):]
-y_val = np.array(y_val, dtype=float)
 print("Taille de l'input d'entraînement : " + str(x_train.shape))
 print("Taille de l'output d'entraînement : " + str(y_train.shape))
-print("Taille de l'input du dataset de validation : " + str(x_val.shape))
-print("Taille de l'output du dataset de validation : " + str(y_val.shape))
-model = build_model(input_dim=128, decoder_input_shape=13, latent_dim=256)
+
+input_dim = 128
+decoder_input_shape = 13
+latent_dim = 256
+
+# Model's input
+input_spectrogram = keras.Input((None, input_dim), name="input_spectrogram")
+# Expand the dimension to use 2D CNN.
+x = layers.Reshape((-1, input_dim, 1), name="expand_dim")(input_spectrogram)
+# Convolution layer 1
+x = layers.Conv2D(
+    filters=32,
+    kernel_size=[11, 41],
+    strides=[2, 2],
+    padding="same",
+    use_bias=False,
+    name="conv_1",
+)(x)
+x = layers.BatchNormalization(name="conv_1_bn")(x)
+x = layers.ReLU(name="conv_1_relu")(x)
+# Convolution layer 2
+x = layers.Conv2D(
+    filters=32,
+    kernel_size=[11, 21],
+    strides=[1, 2],
+    padding="same",
+    use_bias=False,
+    name="conv_2",
+)(x)
+x = layers.BatchNormalization(name="conv_2_bn")(x)
+x = layers.ReLU(name="conv_2_relu")(x)
+# Reshape the resulted volume to feed the RNNs layers
+x = layers.Reshape((-1, x.shape[-2] * x.shape[-1]))(x)
+
+encoder = keras.layers.LSTM(latent_dim, return_state=True)
+encoder_outputs, state_h, state_c = encoder(x)
+
+# We discard `encoder_outputs` and only keep the states.
+encoder_states = [state_h, state_c]
+
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = keras.Input(shape=(None, decoder_input_shape), name="input_teacher_forcing")
+
+# We set up our decoder to return full output sequences,
+# and to return internal states as well. We don't use the
+# return states in the training model, but we will use them in inference.
+decoder_lstm = keras.layers.LSTM(latent_dim, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+decoder_dense = layers.Dense(13, activation='relu')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+# Define the model that will turn
+# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+model = keras.Model([input_spectrogram, decoder_inputs], decoder_outputs)
 
 model.compile(
     optimizer="adam",
     loss=keras.losses.MeanAbsoluteError(),
 )
-model.summary()
+# model.summary()
 
-model.fit(
-    [x_train, decoder_input],
-    y_train,
-    batch_size=64,
-    epochs=5,
-)
+
+# model.fit(
+#     [x_train, decoder_input],
+#     y_train,
+#     batch_size=1,
+#     epochs=50,
+#     # validation_split=0.2
+# )
+
 # Save model
-model.save("OsuMapCreator")
+model.save("MapCreator")
+print("Sauvegarde du modèle terminée")
